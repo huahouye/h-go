9b0eaded4508547a55dafeb26a5c9d377b0f79f8
huahouye <huahouye@DESKTOP-957QMJA>
2017-07-27 20:40:13 +0800

    添加 单方向的channel类型 示例

--- chapters/chapter8/pipeline2/pipeline2.go
+++ chapters/chapter8/pipeline2/pipeline2.go
@@ -3,7 +3,7 @@ Go语言的类型系统提供了单方向的channel类型，分别用于
 只发送或只接收的channel。类型 chan<- int  表示一个只发送int的channel，只能发送不能接
 收。相反，类型 <-chan int  表示一个只接收int的channel，只能接收不能发送。（箭头 <-  和
 关键字chan的相对位置表明了channel的方向。）这种限制将在编译期检测。
--- <- 所在位置 左收右发
+-- <- 所在位置 左收右发，以 goroutine 为中心
 */
 package main
 
